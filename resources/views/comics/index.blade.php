0fbd128181de95fe984cefa1f51fb3df2a243e92
MyUsername <my@email.com>
2023-02-13 18:42:25 +0100

    Print on dom with table

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,17 +5,32 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
    <title>Document</title>
+
+   @vite('resources/js/app.js')
 </head>
 <body>
    
 
 
+   <table class="table">
+      <thead>
+            <tr>
+               <th scope="col">Id</th>
+               <th scope="col">Title</th>
+               <th scope="col">Price</th>
+               <th scope="col">Sale Date</th>
+            </tr>
+      </thead>
 
       @foreach ($comics as $comic)
-
-          <h1>
-            {{$comic->title}}
-          </h1>
-      @endforeach
+      <tbody>
+            <tr>
+               <td>{{ $comic->id }}</td>
+               <td>{{ $comic->title }}</td>
+               <td>{{ $comic->price }}</td>
+               <td>{{ $comic->sale_date }}</td>
+            </tr>
+      </tbody>
+      @endforeach 
 </body>
 </html>
