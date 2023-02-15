b4c609afbef72c803b52a398bb3203dc6c3a7866
MyUsername <my@email.com>
2023-02-15 15:49:35 +0100

    add value on input of form

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -13,49 +13,49 @@
       <label class="form-label">
          Title
       </label>
-      <input type="text" class="form-control" name="title">
+      <input type="text" class="form-control" name="title" value="{{$comic->title}}">
    </div>
 
    <div class="mb-3">
       <label class="form-label">
          description
       </label>
-      <input type="text" class="form-control" name="description">
+      <input type="text" class="form-control" name="description" value="{{$comic->description}}">
    </div>
 
    <div class="mb-3">
       <label class="form-label">
          price
       </label>
-      <input type="text" class="form-control" name="price">
+      <input type="text" class="form-control" name="price" value="{{$comic->price}}">
    </div>
 
    <div class="mb-3">
       <label class="form-label">
          thumb
       </label>
-      <input type="text" class="form-control" name="thumb">
+      <input type="text" class="form-control" name="thumb" value="{{$comic->thumb}}">
    </div>
 
    <div class="mb-3">
       <label class="form-label">
          series
       </label>
-      <input type="text" class="form-control" name="series">
+      <input type="text" class="form-control" name="series" value="{{$comic->series}}">
    </div>
 
    <div class="mb-3">
       <label class="form-label">
          sale_date
       </label>
-      <input type="text" class="form-control" name="sale_date">
+      <input type="text" class="form-control" name="sale_date" value="{{$comic->sale_date}}">
    </div>
 
    <div class="mb-3">
       <label class="form-label">
          type
       </label>
-      <input type="text" class="form-control" name="type">
+      <input type="text" class="form-control" name="type" value="{{$comic->type}}">
    </div>
 
       <button type="submit" class="btn btn-warning">Create new Comics!</button>
